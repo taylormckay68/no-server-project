633d483241722f53e09db82e287f142386c70bd2
Taylor McKay <taylormckay68@gmail.com>
2017-05-29 14:15:48 -0600

    removing s from https

--- js/services/mainSvc.js
+++ js/services/mainSvc.js
@@ -2,7 +2,7 @@ angular.module('noserver').service('mainSvc', function ($http, config) {
 
 
     this.getData = function () {
-        return $http.get(`https://api.wunderground.com/api/${config.API_key}/geolookup/conditions/hourly/forecast/forecast10day/webcams/animatedradar/satellite/q/autoip.json`)
+        return $http.get(`http://api.wunderground.com/api/${config.API_key}/geolookup/conditions/hourly/forecast/forecast10day/webcams/animatedradar/satellite/q/autoip.json`)
         // return $http.get('../../api.json')
             
     }
